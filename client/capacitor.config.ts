56c9d702746715bd48a4e47462116094d2182f98
annazoi <zoianna01@gmail.com>
2024-03-06 00:29:28 +0200

    runnign api on android

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,12 +1,16 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myApp',
-  webDir: 'dist',
+  appId: "io.ionic.starter",
+  appName: "mesge",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "http",
+    allowNavigation: ["localhost", "127.0.0.1", "192.168.1.6"],
+  },
+  android: {
+    allowMixedContent: true,
+  },
 };
 
 export default config;
--- client/dist/assets/swipe-back-legacy-9964a2d8.js
+++ client/dist/assets/swipe-back-legacy-9964a2d8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ab641fcc.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-37cebcaf.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
