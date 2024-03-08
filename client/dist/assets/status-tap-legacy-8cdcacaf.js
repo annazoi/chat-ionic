4e0075a0570cabc0a79b25fd3ad09f0a667a5cba
annazoi <zoianna01@gmail.com>
2024-03-08 15:31:25 +0200

    fix

--- client/dist/assets/status-tap-legacy-8cdcacaf.js
+++ client/dist/assets/status-tap-legacy-8cdcacaf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-07a0a49b.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-c6a2e1d9.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- client/dist/assets/swipe-back-legacy-abc32c17.js
+++ client/dist/assets/swipe-back-legacy-abc32c17.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-07a0a49b.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-c6a2e1d9.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
